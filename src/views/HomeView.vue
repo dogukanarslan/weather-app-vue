c2d0475b87e88b53777ece911c3675b5fd2146a1
Dogukan Arslan <dogukan2arslan@hotmail.com>
2023-12-27 00:48:20 +0300

    Fetch weather by cities

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,59 @@
 <template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        @input="getSearchResults"
+        v-model="searchQuery"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none"
+      />
+      <p v-if="searchError">{{ searchError }}</p>
+      <p v-if="!searchError && searchResults?.length === 0">No results found</p>
+      <ul
+        v-if="searchResults && searchResults.length !== 0"
+        class="absolute bg-weather-secondary text-white shadow-md py-2 px-1 w-full"
+      >
+        <li
+          v-for="result in searchResults"
+          class="bg-weather-secondary p-2 rounded-md"
+        >
+          {{ result.country }}, {{ result.state }}, {{ result.name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+import axios from "axios"
+
+const searchQuery = ref("")
+const queryTimeout = ref(null)
+const searchResults = ref(null)
+const searchError = ref(null)
+
+const API_KEY = "889d20dfd8044353daf28cd4fd544a6d"
+console.log(import.meta.env.API_KEY)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=10&appid=${import.meta.env.VITE_API_KEY}`
+        )
+        searchResults.value = result.data
+      } catch {
+        searchError.value = "An error happened"
+      }
+
+      return
+    }
+
+    searchResults.value = null
+  }, 300)
+}
+</script>
